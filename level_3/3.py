be90a8aceb7e14745bb81595a031c3df6df24a2c
Beloded1 <alexandrbeloded@gmail.com>
2023-06-02 10:44:11 +0400

    Fix 3.py function

--- level_3/3.py
+++ level_3/3.py
@@ -1,18 +1,18 @@
 from constants import ___
+import typing
 
-
-def create_user(user_name: str, user_age: int, after_created: callable) -> None:
+def create_user(user_name: str, user_age: int, after_created: typing.Callable[[int, str], None]) -> None:
     user_id = 111 
     after_created(user_id, user_name)
+   
 
-
-def send_test_email(user_id: int) -> None:
-    print(f"Sending test email to user {user_id}")
+def send_test_email(user_id: int, user_name: str) -> None:
+    print(f"Sending test email to user {user_id} - {user_name}")
 
 
 if __name__ == "__main__":
-    assert create_user(
+    create_user(
         user_name="Ilya",
         user_age=32,
         after_created=send_test_email,
-    ) is None
+    ) 
